2aa76af599fa5ec597046ba350d76ac3dc9066bc
prideandjuice <elvirarahma07@gmail.com>
2024-11-14 00:07:20 +0700

    first commit

---
+++ ClassObject.java
@@ -0,0 +1,20 @@
+class Warung{
+    String namaBarang;
+    int harga;
+
+    void MauBeli(){
+        System.out.println("buuuu beli " + namaBarang +" bu" );
+        System.out.println("iyaaaaaa ambil aja dek harganya" + harga);
+}
+}
+
+public class ClassObject{
+    public static void main(String [] args){
+        Warung warung1 = new Warung();
+
+        warung1.namaBarang = "chocolatos";
+        warung1.harga = 1000;
+
+        warung1.MauBeli();
+    }
+}

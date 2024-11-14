d1d590b81ef9e9170854b722714705ee23a89b32
prideandjuice <elvirarahma07@gmail.com>
2024-11-14 12:24:24 +0700

    ClassObject

--- ClassObject.java
+++ ClassObject.java
@@ -1,21 +1,24 @@
 class Warung{
-    //masukkan variabe
+    //ini disebut atribut
     String namaBarang;
     int harga;
 
     void MauBeli(){
         System.out.println("buuuu beli " + namaBarang +" bu" );
-        System.out.println("iyaaaaaa ambil aja dek harganya" + harga);
+        System.out.println("iyaaaaaa ambil aja dek harganya " + harga);
 }
 }
 
 public class ClassObject{
     public static void main(String [] args){
+        //buat object dari class warung
         Warung warung1 = new Warung();
 
+        // mengisi atribut object tadi
         warung1.namaBarang = "chocolatos";
         warung1.harga = 1000;
 
+        //memanggil method 
         warung1.MauBeli();
     }
 }
